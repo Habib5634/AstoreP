aaf81db099c781eed3335c93abdbe5ffa1b8ddad
habib5634 <hero5276311@gmail.com>
2023-10-18 13:54:40 +0500

    updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,12 @@
   import React, { useState } from 'react';
 import { handleLogin } from '../../services/authService';
-
+import { AiFillEye, AiFillEyeInvisible} from 'react-icons/ai';
  
   const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false); // Add loading state
-
+    const [isPasswordHidden, setPasswordHidden] = useState(true);
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true); // Set loading to true when the form is submitted
@@ -43,14 +43,14 @@ import { handleLogin } from '../../services/authService';
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div>
+              <div className='relative'>
                 <label htmlFor="password" className="sr-only">
                   Password
                 </label>
-                <input
+                <input    
                   id="password"
                   name="password"
-                  type="password"
+                  type={isPasswordHidden ? 'password' : 'text'}
                   autoComplete="current-password"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
@@ -58,6 +58,16 @@ import { handleLogin } from '../../services/authService';
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
+                <span
+              className="text-gray-500 mt-1.5  absolute right-3 inset-y-0 my-auto active:text-gray-500"
+              onClick={() => setPasswordHidden(!isPasswordHidden)}
+            >
+              {isPasswordHidden ? (
+                <AiFillEyeInvisible className="w-6 h-6" />
+              ) : (
+                <AiFillEye className="w-6 h-6" />
+              )}
+            </span>
               </div>
             </div>
 
